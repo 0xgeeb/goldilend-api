6bc80c9703338ecda4ee3342ebafcd86d1ea58c0
0xgeeb <0xgeeb@gmail.com>
2025-08-06 14:41:47 -0400

    tested stuff

--- test.sh
+++ test.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 # test rawdog addy
-curl -X GET "http://localhost:3001/ownedBeras/0x8FE7E03B5b2E49E3386BE79f0834B4B6D08E095c" | jq '.'
-# curl -X GET "http://ec2-98-81-96-90.compute-1.amazonaws.com" | jq '.'
+# curl -X GET "http://localhost:3001/ownedBeras/0x8FE7E03B5b2E49E3386BE79f0834B4B6D08E095c" | jq '.'
+# curl -X GET "http://localhost:3001/ownedBeras/0xe9909fEE970E6f5ffc538B1c384290deB03Be020" | jq '.'
+
+curl -X GET "http://98.81.96.90:3001/ownedBeras/0x8FE7E03B5b2E49E3386BE79f0834B4B6D08E095c" | jq '.'
+# curl -X GET "http://ec2-98-81-96-90.compute-1.amazonaws.com/ownedBeras/0xe9909fEE970E6f5ffc538B1c384290deB03Be020" | jq '.' 
