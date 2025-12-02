1508446fc5fb4f651a4ce07f4e0a10f0858eb41f
0xgeeb <0xgeeb@gmail.com>
2025-12-02 14:31:22 -0500

    testlive

--- scripts/testlive.sh
+++ scripts/testlive.sh
@@ -3,7 +3,7 @@
 # Test live Goldilend API endpoints
 
 # Test with a sample address (update YOUR_DOMAIN_HERE)
-curl -X GET "https://YOUR_DOMAIN_HERE/ownedBeras/0xYourAddressHere" | jq '.'
+curl -X GET "http://98.81.96.90:3001/ownedBeras/0x8FE7E03B5b2E49E3386BE79f0834B4B6D08E095c" | jq '.'
 
 # Test with another address (uncomment to use)
-# curl -X GET "https://YOUR_DOMAIN_HERE/ownedBeras/0xAnotherAddress" | jq '.'
+# curl -X GET "http://98.81.96.90:3001/ownedBeras/0xAnotherAddress" | jq '.'
