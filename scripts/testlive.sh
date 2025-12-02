c99e2fc031d1debb0d1ed6460da937e746a1b271
0xgeeb <0xgeeb@gmail.com>
2025-12-02 12:01:55 -0500

    delete scripts

--- scripts/testlive.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Test live Goldilend API endpoints
-
-# Test with a sample address (update YOUR_DOMAIN_HERE)
-curl -X GET "https://YOUR_DOMAIN_HERE/ownedBeras/0xYourAddressHere" | jq '.'
-
-# Test with another address (uncomment to use)
-# curl -X GET "https://YOUR_DOMAIN_HERE/ownedBeras/0xAnotherAddress" | jq '.'
--- scripts/testlocal.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Test local Goldilend API endpoints
-
-# Test with a sample address
-curl -X GET "http://localhost:3001/ownedBeras/0xYourAddressHere" | jq '.'
-
-# Test with another address (uncomment to use)
-# curl -X GET "http://localhost:3001/ownedBeras/0xAnotherAddress" | jq '.'
